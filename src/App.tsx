cfddddddf8129f08bb66c30a3aee445f790a531f
h-nriquevieira <h.nriquevieira@gmail.com>
2023-09-18 18:12:22 -0300

    feat: render prop version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { CssBaseline } from "@mui/material";
 import "./App.css";
+import DataTableRenderer from "./components/DataTableRenderer/DataTableRenderer";
+import UserTable from "./components/UserTable/UserTable";
 
 function App() {
   return (
     <>
       <CssBaseline />
       React composition patterns
+      <DataTableRenderer render={UserTable} />
     </>
   );
 }
