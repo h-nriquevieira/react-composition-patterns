a4eab64bc425c3031d4eddd5bc0a4a110d0a61a8
h-nriquevieira <h.nriquevieira@gmail.com>
2023-09-18 18:05:14 -0300

    fix: cleanup App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { CssBaseline } from "@mui/material";
 import "./App.css";
-import { HOCEnhancedUserTable } from "./components/HOCEnhancedUserTable/HOCEnhancedUserTable";
 
 function App() {
   return (
