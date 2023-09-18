d4bf630974cc247d7d5286d3f87b2b9707969a88
h-nriquevieira <h.nriquevieira@gmail.com>
2023-09-18 17:58:58 -0300

    feat: style UserTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { CssBaseline } from "@mui/material";
 import "./App.css";
 import { HOCEnhancedUserTable } from "./components/HOCEnhancedUserTable/HOCEnhancedUserTable";
 
 function App() {
   return (
     <>
+      <CssBaseline />
       <HOCEnhancedUserTable />
     </>
   );
--- src/components/UserTable/UserTable.tsx
+++ src/components/UserTable/UserTable.tsx
@@ -1,13 +1,34 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { DataTableProps } from "../../types/DataTableProps";
 
 export default function UserTable({ data }: DataTableProps) {
   return (
-    <>
-      {data.map((item) => (
-        <p>
-          {item.name} + {item.age} + {item.isActive ? "Ativo" : "Inativo"}{" "}
-        </p>
-      ))}
-    </>
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Nome</TableCell>
+            <TableCell>Idade</TableCell>
+            <TableCell>Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((user) => (
+            <TableRow>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.age}</TableCell>
+              <TableCell>{user.isActive ? "Ativo" : "Inativo"}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
