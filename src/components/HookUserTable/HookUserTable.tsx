7555e261aca9c9101b1bf17eb5c557acc0791b7b
h-nriquevieira <h.nriquevieira@gmail.com>
2023-09-19 17:35:03 -0300

    refactor: remove duplication of UserTable

--- src/components/HookUserTable/HookUserTable.tsx
+++ src/components/HookUserTable/HookUserTable.tsx
@@ -1,44 +1,16 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TextField,
-} from "@mui/material";
 import useDataTable from "../../hooks/useDataTable";
+import UserTable from "../UserTable/UserTable";
 
 export default function HookUserTable() {
   const { data, filter, handleFilterChange } = useDataTable();
 
   return (
     <>
-      <TextField
-        label="Filtrar pelo nome"
-        value={filter}
-        onChange={handleFilterChange}
+      <UserTable
+        data={data}
+        filter={filter}
+        handleFilterChange={handleFilterChange}
       />
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Nome</TableCell>
-              <TableCell>Idade</TableCell>
-              <TableCell>Status</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((user) => (
-              <TableRow>
-                <TableCell>{user.name}</TableCell>
-                <TableCell>{user.age}</TableCell>
-                <TableCell>{user.isActive ? "Ativo" : "Inativo"}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
     </>
   );
 }
